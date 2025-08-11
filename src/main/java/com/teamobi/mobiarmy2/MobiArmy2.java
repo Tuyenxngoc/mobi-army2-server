c8544a215741b4836c8dd63b029e2ff09fe13499
Trinh Ngoc Tuyen <tuyentn@3tit.vn>
2025-08-11 11:50:22 +0700

    ✨feat: refactor bean registration logic into BeanRegistry for improved organization and maintainability

--- src/main/java/com/teamobi/mobiarmy2/MobiArmy2.java
+++ src/main/java/com/teamobi/mobiarmy2/MobiArmy2.java
@@ -1,11 +1,13 @@
 package com.teamobi.mobiarmy2;
 
 import com.teamobi.mobiarmy2.server.ApplicationContext;
+import com.teamobi.mobiarmy2.server.BeanRegistry;
 import com.teamobi.mobiarmy2.server.ServerManager;
 import com.teamobi.mobiarmy2.ui.ServerUI;
 
 public class MobiArmy2 {
     public static void main(String[] args) {
+        BeanRegistry.registerBeans();
         ServerManager serverManager = ApplicationContext.getInstance().getBean(ServerManager.class);
         Runtime.getRuntime().addShutdownHook(new Thread(serverManager::stop, "ServerShutdownHook"));
 
