7fed2647e804a1737b4b10b449b9b3bcc1ad0047
Trinh Ngoc Tuyen <tuyentn@3tit.vn>
2026-01-27 16:31:42 +0700

    Add LocalDate support to GsonUtil with custom serializer and deserializer

--- src/main/java/com/teamobi/mobiarmy2/util/GsonUtil.java
+++ src/main/java/com/teamobi/mobiarmy2/util/GsonUtil.java
@@ -3,15 +3,23 @@ package com.teamobi.mobiarmy2.util;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 public class GsonUtil {
     private static final Gson INSTANCE = createGson();
 
+    private static final DateTimeFormatter ISO_DATE_TIME =
+            DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    private static final DateTimeFormatter ISO_DATE =
+            DateTimeFormatter.ISO_LOCAL_DATE;
+
     private static Gson createGson() {
         return new GsonBuilder()
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
                 .create();
     }
 
@@ -19,18 +27,47 @@ public class GsonUtil {
         return INSTANCE;
     }
 
-    private static class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+    private static class LocalDateTimeAdapter
+            implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+
         @Override
-        public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
-            long epochMilli = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
-            return new JsonPrimitive(epochMilli);
+        public JsonElement serialize(
+                LocalDateTime localDateTime,
+                Type type,
+                JsonSerializationContext context
+        ) {
+            return new JsonPrimitive(localDateTime.format(ISO_DATE_TIME));
         }
 
         @Override
-        public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-                throws JsonParseException {
-            long epochMilli = json.getAsLong();
-            return LocalDateTime.ofEpochSecond(epochMilli / 1000, (int) (epochMilli % 1000) * 1_000_000, ZoneOffset.UTC);
+        public LocalDateTime deserialize(
+                JsonElement json,
+                Type typeOfT,
+                JsonDeserializationContext context
+        ) throws JsonParseException {
+            return LocalDateTime.parse(json.getAsString(), ISO_DATE_TIME);
+        }
+    }
+
+    private static class LocalDateAdapter
+            implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+
+        @Override
+        public JsonElement serialize(
+                LocalDate value,
+                Type type,
+                JsonSerializationContext context
+        ) {
+            return new JsonPrimitive(value.format(ISO_DATE));
+        }
+
+        @Override
+        public LocalDate deserialize(
+                JsonElement json,
+                Type typeOfT,
+                JsonDeserializationContext context
+        ) throws JsonParseException {
+            return LocalDate.parse(json.getAsString(), ISO_DATE);
         }
     }
 }
